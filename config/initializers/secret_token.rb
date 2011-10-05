e7a2c260389314387ed05ffde70f1106c4c5c346
Scott Cytacki <scytacki@concord.org>
2011-10-05 09:45:54 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HoboMatrix::Application.config.secret_token = '4f422a1d5597e1e7b4a0c36998c0985e4a30b8b3a4b85c86ecf935f4402c31eaa9e0275db328e65bdb6292379b9522a5b5fb481ebabb6f5ca5d3847b3679c174'
